df02c0beea34dbda2b1f1b82552adefbde2a5abc
jeffreyshaffer <jeff.shaffer@rocketmbsoft.com>
2011-05-17 17:45:40 -0400

    Added

---
+++ HoneyDooShare/src/com/rocketmbsoft/honeydoo/s/SharedData.java
@@ -0,0 +1,6 @@
+package com.rocketmbsoft.honeydoo.s;
+
+public final class SharedData {
+	public static final String HONEY_DOO_WEBSERVER = "http://1.rocketmbsofthoneydoo.appspot.com/";
+	public static final String HONEY_DOO_WEBSITE = HONEY_DOO_WEBSERVER+"welcome";
+}
